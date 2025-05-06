edbd2c0d41874ff7c3a7b374b2e2c1dfb8cd3887
effeto <effectdopler@gmail.com>
2025-05-06 10:47:51 +0300

    Initial Commit

---
+++ AppflameTestTask/AppflameTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppflameTestTaskApp.swift
+//  AppflameTestTask
+//
+//  Created by Демьян on 06.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct AppflameTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
